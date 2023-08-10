8b5117e92e32c4eda0892f98c06e78e591cd047d
web2henrique047 <web2henrique047@gmail.com>
2023-08-10 09:32:18 -0300

    ADD classes Quadrante (I)

---
+++ Quadrante/src/Coord.java
@@ -0,0 +1,4 @@
+public class Coord {
+    private float x = 0f;
+    private float y = 0f;
+}
---
+++ Quadrante/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("Coordenada: ");
+        Coord ponto = new Coord();
+
+        
+
+        read.close();
+    }
+}
