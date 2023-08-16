679a757ee3a6bfc808945d2c21fae4f442d1d1ed
web2henrique047 <web2henrique047@gmail.com>
2023-08-16 14:28:25 -0300

    ADD EntreComTresNum (C)

---
+++ EntreComTresNum/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Insira 3 números: ");
+        int a = read.nextInt();
+        int b = read.nextInt();
+        int c = read.nextInt();
+
+        if (a > b && a > c) {
+            System.out.printf("Maior: %d%n", a);
+        } else if (b > c) {
+            System.out.printf("Maior: %d%n", b);
+        } else {
+            System.out.printf("Maior: %d%n", c);
+        }
+        
+        read.close();
+    }
+}
