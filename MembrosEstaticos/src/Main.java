067271028f84406a3d552ba875f4e6247b5c32e5
web2henrique047 <web2henrique047@gmail.com>
2023-08-16 15:14:54 -0300

    ADD MembrosEstaticos (C)

---
+++ MembrosEstaticos/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculadora;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner read = new Scanner(System.in);
+        
+        System.out.println("Insira o raio da circunferência: ");
+        double raio = read.nextDouble();
+
+        System.out.printf("Circunferência: %.2f%n", Calculadora.circunferencia(raio));
+        System.out.printf("Volume: %.2f%n", Calculadora.valume(raio));
+        System.out.printf("Valor de PI: %.5f%n", Calculadora.PI);
+        
+        read.close();
+    }
+}
