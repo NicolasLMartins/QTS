979942dac40d67413ca47f69864d8ab323dc1ec9
NicolasLMartins <nicolaslm2006@gmail.com>
2023-10-18 14:32:40 -0300

    UPDATE Reservation

--- Aula1011/TratamentoDeExcecoes/PousadaBom/src/model/entities/Reservation.java
+++ Aula1011/TratamentoDeExcecoes/PousadaBom/src/model/entities/Reservation.java
@@ -39,26 +39,19 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    // public void updateDates (Date checkIn, Date checkOut) {
-    //     this.checkIn = checkIn;
-    //     this.checkOut = checkOut;
-    // }
-    
-    public String updateDates (Date checkIn, Date checkOut) {
+    public void updateDates (Date checkIn, Date checkOut) {
         Date now = new Date();
 
         if (checkIn.before(now) || checkOut.before(now)) {
-            return "Reservation dates must be future dates";
+            throw new IllegalArgumentException("Reservation dates must be future dates");
         }
 
         if (!checkOut.after(checkIn)) {
-            return "Check-out date must be after check-in date.";
+            throw new IllegalArgumentException("Check-out date must be after check-in date.");
         }
 
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-
-        return null;
     }
 
     @Override
