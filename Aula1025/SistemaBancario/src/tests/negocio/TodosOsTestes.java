8752c04e3594143ebb738ee2ac09e76674329292
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-01 15:51:54 -0300

    ADD TodosOsTestes

---
+++ Aula1025/SistemaBancario/src/tests/negocio/TodosOsTestes.java
@@ -0,0 +1,16 @@
+package tests.negocio;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(
+    {
+        GerenciadorClientesTest_Ex01.class, GerenciadorClientesTest_Ex02.class, GerenciadorClientesTest_Ex03.class, 
+        GerenciadorClientesTest_Ex04.class, GerenciadorContasTest_Ex01.class, GerenciadorContasTest_Ex02.class, 
+        GerenciadorContasTest_Ex03.class, GerenciadorContasTest_Ex04.class
+    }
+)
+
+public class TodosOsTestes {}
