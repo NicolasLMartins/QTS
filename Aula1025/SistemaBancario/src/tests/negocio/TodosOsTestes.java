2dd68ed90bd89afe590bf1c3c47a075f7cd6232d
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-08 14:25:41 -0300

    ADD GerenciadorClientesTest_Ex05

--- Aula1025/SistemaBancario/src/tests/negocio/TodosOsTestes.java
+++ Aula1025/SistemaBancario/src/tests/negocio/TodosOsTestes.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses(
     {
         GerenciadorClientesTest_Ex01.class, GerenciadorClientesTest_Ex02.class, GerenciadorClientesTest_Ex03.class, 
-        GerenciadorClientesTest_Ex04.class, GerenciadorContasTest_Ex01.class, GerenciadorContasTest_Ex02.class, 
+        GerenciadorClientesTest_Ex04.class, GerenciadorClientesTest_Ex05.class, GerenciadorContasTest_Ex01.class, GerenciadorContasTest_Ex02.class, 
         GerenciadorContasTest_Ex03.class, GerenciadorContasTest_Ex04.class
     }
 )
