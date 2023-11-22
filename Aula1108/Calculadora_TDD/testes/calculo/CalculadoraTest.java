b4eecc927e894609a69c558b1a20e61264ffa8af
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-22 14:29:31 -0300

    ADD other operation tests

--- Aula1108/Calculadora_TDD/testes/calculo/CalculadoraTest.java
+++ Aula1108/Calculadora_TDD/testes/calculo/CalculadoraTest.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import exceptions.CantDivideByZeroException;
+
 public class CalculadoraTest {
     
+    // Tests if the Calculadora's method sum's result is correct
     @Test
     public void mustSumTwoValues() {
         //CENÁRIO
@@ -15,9 +18,89 @@ public class CalculadoraTest {
         Calculadora calc = new Calculadora();
     
         //EXECUÇÃO
-        int resultado = calc.soma(a, b);
+        int resultado = calc.sum(a, b);
     
         //VERIFICAÇÃO
         assertEquals(12, resultado);
+    }
+
+    // Tests if the Calculadora's method subtract's result is correct
+    @Test
+    public void mustSubtractTwoValues() {
+        //CENÁRIO
+        int a = 10;
+        int b = 2;
+
+        Calculadora calc = new Calculadora();
+    
+        //EXECUÇÃO
+        int resultado = calc.subtract(a, b);
+    
+        //VERIFICAÇÃO
+        assertEquals(8, resultado);
+    }
+
+    // Tests if the Calculadora's method multiply's result is correct
+    @Test
+    public void mustMultiplyTwoValues() {
+        //CENÁRIO
+        int a = 10;
+        int b = 2;
+
+        Calculadora calc = new Calculadora();
+    
+        //EXECUÇÃO
+        int resultado = calc.multiply(a, b);
+    
+        //VERIFICAÇÃO
+        assertEquals(20, resultado);
+    }
+
+    // Tests if the Calculadora's method divide's result is correct
+    @Test
+    public void mustDivideTwoValues() throws CantDivideByZeroException {
+        //CENÁRIO
+        int a = 10;
+        int b = 2;
+
+        Calculadora calc = new Calculadora();
+    
+        //EXECUÇÃO
+        int resultado = calc.divide(a, b);
+    
+        //VERIFICAÇÃO
+        assertEquals(5, resultado);
+    }
+
+    // Tests if the Calculadora's method divRemainder's result is correct
+    @Test
+    public void mustResultDivisionRemainder() {
+        //CENÁRIO
+        int a = 10;
+        int b = 2;
+
+        Calculadora calc = new Calculadora();
+    
+        //EXECUÇÃO
+        int resultado = calc.divRemainder(a, b);
+    
+        //VERIFICAÇÃO
+        assertEquals(0, resultado);
+    }
+
+    @Test(expected = CantDivideByZeroException.class)
+    public void mustThrowExceptionDivideByZero() throws CantDivideByZeroException {
+        //CENÁRIO
+                //CENÁRIO
+        int a = 10;
+        int b = 0;
+
+        Calculadora calc = new Calculadora();
+    
+        //EXECUÇÃO
+        int resultado = calc.divide(a, b);
+    
+        //VERIFICAÇÃO
+        assertEquals(5, resultado);
     }
 }
