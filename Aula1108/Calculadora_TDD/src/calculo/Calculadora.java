bbdadf54a5234c9c00408ca84c96c40bc07ab825
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-22 14:29:45 -0300

    ADD other operations methods

--- Aula1108/Calculadora_TDD/src/calculo/Calculadora.java
+++ Aula1108/Calculadora_TDD/src/calculo/Calculadora.java
@@ -1,9 +1,30 @@
 package calculo;
 
+import exceptions.CantDivideByZeroException;
+
 public class Calculadora {
 
-    public int soma(int a, int b) {
+    public int sum(int a, int b) {
         return a + b;
     }
 
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) throws CantDivideByZeroException {
+        if (b == 0) {
+            throw new CantDivideByZeroException();
+        }
+        
+        return a / b;
+    }
+
+    public int divRemainder(int a, int b) {
+        return a % b;
+    }
 }
