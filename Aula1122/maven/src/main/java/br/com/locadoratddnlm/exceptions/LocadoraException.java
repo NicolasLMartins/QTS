582d71a53509aebd05379d0399a594f8b50c096f
NicolasLMartins <nicolaslm2006@gmail.com>
2023-11-29 15:03:44 -0300

    ADD LocadoraException exception

---
+++ Aula1122/maven/src/main/java/br/com/locadoratddnlm/exceptions/LocadoraException.java
@@ -0,0 +1,9 @@
+package br.com.locadoratddnlm.exceptions;
+
+public class LocadoraException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public LocadoraException(String message) {
+        super(message);
+    }
+}
